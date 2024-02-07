a8c6dd32ac62a9f94236056f35ca172c2ebd91f5
rxn7 <rotthin_dev@protonmail.com>
2024-02-07 19:18:15 +0100

    Added mobile controls

---
+++ scripts/mobile_controls.gd
@@ -0,0 +1,34 @@
+class_name MobileControls
+extends Node
+
+const SWIPE_THRESHOLD = 100
+
+var swipe_start_position: Vector2
+var swiping: bool = false
+
+func _input(event: InputEvent):
+	if event is InputEventScreenTouch:
+		if swiping:
+			return
+
+		if event.is_pressed():
+			swiping = true
+			swipe_start_position = event.position
+
+	elif event is InputEventScreenDrag:
+		if !swiping:
+			return
+
+		var swipe: Vector2 = event.position - swipe_start_position
+		if abs(swipe.x) > SWIPE_THRESHOLD:
+			swiping = false
+			if swipe.x > 0:
+				Game.instance.slide_right()
+			else:
+				Game.instance.slide_left()
+		elif abs(swipe.y) > SWIPE_THRESHOLD:
+			swiping = false
+			if swipe.y < 0:
+				Game.instance.slide_up()
+			else:
+				Game.instance.slide_down()
